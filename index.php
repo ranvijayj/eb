32abcade0cece1d5af87f58806c5245f29310247
ranvijayj <ranvijayj@intelligrape.com>
2015-04-14 12:15:46 +0530

    done

--- index.php
+++ index.php
@@ -1,8 +1,17 @@
-<html>
- <head>
-  <title>PHP Test</title>
- </head>
- <body>
- <?php echo '<p>another test hello World how are you</p>'; ?> 
- </body>
-</html>		
+<?php
+/**
+ * Front to the WordPress application. This file doesn't do anything, but loads
+ * wp-blog-header.php which does and tells WordPress to load the theme.
+ *
+ * @package WordPress
+ */
+
+/**
+ * Tells WordPress to load the WordPress theme and output it.
+ *
+ * @var bool
+ */
+define('WP_USE_THEMES', true);
+
+/** Loads the WordPress Environment and Template */
+require( dirname( __FILE__ ) . '/wp-blog-header.php' );
